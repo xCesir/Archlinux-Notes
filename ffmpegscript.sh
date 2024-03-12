e8e7ba890b0c0b695eb7f96e6a637b0144327218
xCesir <78450857+xCesir@users.noreply.github.com>
2024-03-12 13:55:09 +0100

    Update ffmpegscript.sh

--- ffmpegscript.sh
+++ ffmpegscript.sh
@@ -7,6 +7,5 @@ do
   b=${f##*/}
   echo "Processing $f file..."
   # take action on each file. $f store current file name
-  ffmpeg -vaapi_device /dev/dri/renderD128 -i "$f" -vf 'format=nv12,hwupload' -c:v av1_vaapi -rc_mode 1 "$path${b%.*}_av1.mkv";
+  ffmpeg -vaapi_device /dev/dri/renderD128 -i "$f" -vf 'format=nv12,hwupload' -c:v av1_vaapi -rc_mode 1 -f matroska "$path${b%.*}_av1.mkv";
 done
-
