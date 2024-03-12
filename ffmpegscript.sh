6f36037d1a4ae9c29d04069d715108b7f4d162fb
xCesir <78450857+xCesir@users.noreply.github.com>
2024-03-12 13:53:02 +0100

    Create ffmpegscript.sh

---
+++ ffmpegscript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# NOTE : Quote it else use array to avoid problems #
+path="/run/media/sidney/HDD/unconverted/test/"
+FILES="$path*"
+for f in $FILES
+do
+  b=${f##*/}
+  echo "Processing $f file..."
+  # take action on each file. $f store current file name
+  ffmpeg -vaapi_device /dev/dri/renderD128 -i "$f" -vf 'format=nv12,hwupload' -c:v av1_vaapi -rc_mode 1 "$path${b%.*}_av1.mkv";
+done
+
