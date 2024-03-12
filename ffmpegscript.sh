b3fa3234b8a6e16bf6856b645cf93d091f303f88
xCesir <78450857+xCesir@users.noreply.github.com>
2024-03-12 17:54:11 +0100

    Update ffmpegscript.sh

--- ffmpegscript.sh
+++ ffmpegscript.sh
@@ -1,4 +1,4 @@
-  GNU nano 7.2                                                                                           sh.sh                                                                                           Modified  
+                                                                                        ffmpeg.sh                                                                                                                 
 #!/bin/bash
 # NOTE : Quote it else use array to avoid problems #
 path="/run/media/sidney/HDD/unconverted/test/"
@@ -7,10 +7,8 @@ for f in $FILES
 do
   b=${f##*/}
   echo "Processing $f file..."
-  # take action on each file. $f store current file name
-  # ffmpeg -vaapi_device /dev/dri/renderD128 -i "$f" -vf 'format=nv12,hwupload' -c:v av1_vaapi -rc_mode 1 -f matroska "$path${b%.*}_av1.mkv";
-  ffmpeg -hwaccel vaapi -hwaccel_device /dev/dri/renderD128 -hwaccel_output_format vaapi -i "$f" -c:v av1_vaapi "$path${b%.*}_av1.mkv";
+  # ffmpeg -vaapi_device /dev/dri/renderD128 -i "$f" -vf 'format=nv12,hwupload' -c:v av1_vaapi -q:v 1 -f matroska "$path${b%.*}_av1.mkv"
+  # ffmpeg -vaapi_device /dev/dri/renderD128 -i "$f" -vf 'format=nv12,hwupload' -c:v av1_vaapi -b:v 1200K -f matroska "$path${b%.*}_av1.mkv"
 
+  # ffmpeg -hwaccel vaapi -hwaccel_device /dev/dri/renderD128 -hwaccel_output_format vaapi -i "$f" -c:v av1_vaapi -b:v 1M -maxrate 2M "$path${b%.*}_av1.mkv"
 done
-
-
