4b9d68abd224f5b2c81bd6fefca9abf91d1e46b3
xCesir <78450857+xCesir@users.noreply.github.com>
2024-09-30 20:14:15 +0200

    Update test.bat

--- ffmpeg/test.bat
+++ ffmpeg/test.bat
@@ -1,1 +1,2 @@
 ffmpeg -hwaccel vaapi -hwaccel_device /dev/dri/renderD128 -hwaccel_output_format vaapi -i "%%a" -c:v av1_vaapi -profile:a 2 -tier 1 -b:v 10.0M -maxrate 10.5M "%%~da%%~pa%%~na_av1.mkv"
+ffmpeg -hwaccel vaapi -hwaccel_device /dev/dri/renderD128 -hwaccel_output_format vaapi -i "%%a" -c:v av1_vaapi -profile:a 2 -tier 1 -rc_mode 1 -global_quality:v 31 "%%~da%%~pa%%~na_av1.mkv"
