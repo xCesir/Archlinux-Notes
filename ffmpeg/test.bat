ab73f0ade18bb47dd3d15a92b1eadb397bee63e2
xCesir <78450857+xCesir@users.noreply.github.com>
2024-09-30 20:05:41 +0200

    Update test.bat

--- ffmpeg/test.bat
+++ ffmpeg/test.bat
@@ -1,17 +1,1 @@
-@echo off
-
-pushd "Z:\"
-(
-  for /r %%a in (*.mov) do (
-  START /W ffmpeg -i "%%a" -c:v av1_qsv -preset 1 -look_ahead_depth 100 "%%~da%%~pa%%~na_av1.mkv"  
-  )
-)
-P
-
-pushd "Z:\"
-(
-  for /r %%a in (*.mp4) do (
-  START /W ffmpeg -i "%%a" -c:v av1_qsv -preset 1 -look_ahead_depth 100 "%%~da%%~pa%%~na_av1.mkv"  
-  )
-)
-PAUSE
+ffmpeg -hwaccel vaapi -hwaccel_device /dev/dri/renderD128 -hwaccel_output_format vaapi -i "%%a" -c:v av1_vaapi -profile:a 2 -tier 1 -b:v 10.0M -maxrate 10.5M "%%~da%%~pa%%~na_av1.mkv"
