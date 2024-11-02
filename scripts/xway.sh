6c0d6aa33fd90793c2884c801f45d0aae0daa977
xCesir <78450857+xCesir@users.noreply.github.com>
2024-11-02 14:21:26 +0100

    up

--- scripts/xway.sh
+++ scripts/xway.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ $XDG_CURRENT_DESKTOP = GNOME ] || [ $XDG_CURRENT_DESKTOP = COSMIC ]
+if [ $XDG_CURRENT_DESKTOP = GNOME ] || [ $XDG_CURRENT_DESKTOP = COSMIC ] || [ $XDG_CURRENT_DESKTOP = KDE ]
 then 
 	pkill "xwaylandvideobr"
 else
