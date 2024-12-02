82d6622efb6ac045ff8c900e0b00baacbc9c036c
xCesir <78450857+xCesir@users.noreply.github.com>
2024-12-02 15:49:38 +0100

    update

---
+++ hypr/ML4W-Hypr/resetTestFiles.sh
@@ -0,0 +1,7 @@
+cp ./.config/hypr/conf/keybindings/default.conf.old ./.config/hypr/conf/keybindings/default
+cp ./.config/hypr/conf/monitors/default.conf.old ./.config/hypr/conf/monitors/default
+cp ./.config/hypr/conf/autostart.conf.old ./.config/hypr/conf/autostart
+cp ./.config/ml4w/settings/system-monitor.sh.old ./.config/ml4w/settings/system-monitor
+cp ./.config/waybar/themes/ml4w/config.old ./.config/waybar/themes/ml4w/configtest
+cp ./.config/waybar/themes/ml4w/style.css.old ./.config/waybar/themes/ml4w/style
+cp ./.config/waybar/modules.json.old ./.config/waybar/modules
