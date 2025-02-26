858c7ec7396c73990a852ed5d38ca0882b7911b0
xCesir <78450857+xCesir@users.noreply.github.com>
2025-02-26 13:27:13 +0100

    Update xway.sh

--- scripts/xway.sh
+++ scripts/xway.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 if [ $XDG_CURRENT_DESKTOP = GNOME ] || [ $XDG_CURRENT_DESKTOP = COSMIC ] || [ $XDG_CURRENT_DESKTOP = KDE ]
 then 
-	pkill "xwaylandvideobr"
+	if pgrep -x "xwaylandvideobr" > /dev/null
+	then
+	    echo "Running"
+	    pkill "xwaylandvideobr"
+	else
+	    echo "Stopped"
+	fi
 else
 	echo "Process not found"
 fi
